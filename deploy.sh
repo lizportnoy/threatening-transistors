88190dedab66c8f63c31a56eac7f395b06e72893
timhua <tim.hua@icloud.com>
2014-12-09 16:39:55 -0800

    added deploy.sh script for bower install

--- deploy.sh
+++ deploy.sh
@@ -136,7 +136,16 @@ fi
 #   exitWithMessageOnError "grunt failed"
 #   cd - > /dev/null
 # fi
-##################################################################################################################################
+
+if [ -e "$DEPLOYMENT_TARGET/Gruntfile.js" ]; then
+  cd "$DEPLOYMENT_TARGET"
+  eval $NPM_CMD install grunt-cli
+  exitWithMessageOnError "installing grunt failed"
+  ./node_modules/.bin/grunt --no-color build
+  exitWithMessageOnError "grunt failed"
+  cd - > /dev/null
+fi
+################################################################################################################################
 
 # Post deployment stub
 if [[ -n "$POST_DEPLOYMENT_ACTION" ]]; then
